62bc1b084d86d3274328778d454ebe2a2f70784f
nullabletypo <masahirokato.dev@gmail.com>
2018-05-24 15:26:47 +0900

    0.1.4

--- dist/command.d.ts
+++ dist/command.d.ts
@@ -5,7 +5,7 @@ export interface Command<T = any> {
     meta?: any;
     [key: string]: any;
 }
-export declare type CommandCreator<P, T = undefined> = T extends undefined | never ? {
+export declare type CommandCreator<P, T> = T extends undefined | never ? {
     type: string;
     (): Command<P>;
 } : {
