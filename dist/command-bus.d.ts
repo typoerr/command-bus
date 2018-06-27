d8a5bc3239dd4942bb6433d7a95629dd83ceaa7c
nullabletypo <masahirokato.dev@gmail.com>
2018-06-27 16:55:28 +0900

    0.2.0

--- dist/command-bus.d.ts
+++ dist/command-bus.d.ts
@@ -9,39 +9,24 @@ export interface CommandCreatorListener<T extends AnyCommandCreator> {
 export declare type CommandBus = ReturnType<typeof createCommandBus>;
 export declare const WILDCARD = "*";
 export declare function createCommandBus(): {
-    dispatch: <T extends {
-        type: string;
-        payload: any;
-    }>(command: T) => T;
+    dispatch: <T extends Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(command: T) => T;
     on: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     off: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
@@ -49,64 +34,40 @@ export declare function createCommandBus(): {
     addEventListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     removeEventListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     addListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     removeListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
         <T extends {
-            (): {
-                type: string;
-                payload: any;
-            };
+            (): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         } | {
-            (value: any): {
-                type: string;
-                payload: any;
-            };
+            (value: any): Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>;
             type: string;
         }>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
--- es/command.d.ts
+++ es/command.d.ts
@@ -1,21 +1,21 @@
 import { HashMap } from '@cotto/utils.ts';
-export declare type Command<P = any, E = {}> = E & {
+export declare type Command<P = any, E = HashMap> = E & {
     type: string;
     payload: P;
 };
-export declare type CommandCreator<T, P, E = {}> = T extends void ? {
+export declare type CommandCreator<T, P, E = HashMap> = T extends void ? {
     type: string;
     (): Command<P, E>;
 } : {
     type: string;
     (value: T): Command<P, E>;
 };
-export declare type AnyCommandCreator<P = any, E = {}> = CommandCreator<any, P, E>;
+export declare type AnyCommandCreator<P = any, E = HashMap> = CommandCreator<any, P, E>;
 export interface CreatorFactory {
     (type: string): CommandCreator<undefined, undefined>;
     <P>(type: string): CommandCreator<P, P>;
-    <P, E extends HashMap>(type: string, payload?: () => P, extra?: () => E): CommandCreator<undefined, P, E>;
-    <T, P, E extends HashMap>(type: string, payload?: (val: T) => P, extra?: (val: T) => E): CommandCreator<T, P, E>;
+    <P, E extends HashMap = HashMap>(type: string, payload?: () => P, extra?: () => E): CommandCreator<undefined, P, E>;
+    <T, P, E extends HashMap = HashMap>(type: string, payload?: (val: T) => P, extra?: (val: T) => E): CommandCreator<T, P, E>;
 }
 export declare function factory(scope: string): CreatorFactory;
 export declare const create: CreatorFactory;
