0ad64e87f7b53e4af52177ba1b1446f20ba6374b
nullabletypo <masahirokato.dev@gmail.com>
2018-07-14 14:49:16 +0900

    0.2.2

--- dist/command-bus.d.ts
+++ dist/command-bus.d.ts
@@ -9,30 +9,30 @@ export interface CommandCreatorListener<T extends AnyCommandCreator> {
 export declare type CommandBus = ReturnType<typeof createCommandBus>;
 export declare const WILDCARD = "*";
 export declare function createCommandBus(): {
-    dispatch: <T extends Command<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(command: T) => T;
+    dispatch: <T extends Command<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(command: T) => T;
     on: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     off: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     getListeners: (target: BusTarget) => Function[];
     addEventListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     removeEventListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     addListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
     removeListener: {
         <T extends string | symbol>(target: T, listener: CommandListener): CommandListener;
-        <T extends AnyCommandCreator<any, import("../../../../../../Users/masahiro/Dev/lib/command-bus/node_modules/@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
+        <T extends AnyCommandCreator<any, import("@cotto/utils.ts/dist/types").HashMap<any>>>(target: T, listener: CommandCreatorListener<T>): CommandCreatorListener<T>;
     };
 };
