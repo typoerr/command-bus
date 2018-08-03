6099c90c1555af2ec541239e84f7dabceb1638fd
nullabletypo <masahirokato.dev@gmail.com>
2018-08-03 11:09:55 +0900

    0.3.7

--- es/command.d.ts
+++ es/command.d.ts
@@ -3,17 +3,17 @@ export declare type Command<P = any, E = HashMap> = E & {
     type: string;
     payload: P;
 };
-export interface CommandCreator<T extends any[], P = undefined, E extends HashMap = {}> {
+export declare type CreatorFunc<T, P = undefined, E extends HashMap = {}> = T extends any[] ? (...input: T) => Command<P, E> : T extends void ? (...input: [...any[]]) => Command<P, E> : (...input: [T, ...any[]]) => Command<P, E>;
+export declare type CommandCreator<T, P = undefined, E extends HashMap = {}> = {
     type: string;
-    (...input: T): Command<P, E>;
-}
+} & CreatorFunc<T, P, E>;
 export interface AnyCommandCreator<P = any, E = {}> {
     type: string;
     (...input: any[]): Command<P, E>;
 }
 export interface CreatorFactory {
-    (type: string): CommandCreator<[undefined?]>;
-    <P>(type: string): CommandCreator<[P], P>;
+    (type: string): CommandCreator<undefined>;
+    <P>(type: string): CommandCreator<P, P>;
     <T extends any[], P, E extends HashMap>(type: string, payload?: (...v: T) => P, extra?: (...v: T) => E): CommandCreator<T, P, E>;
 }
 export declare function factory(scope: string): CreatorFactory;
