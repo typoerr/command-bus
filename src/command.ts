f349c13a9cd241548073a9f4bc54f4492f2ce69e
typoerr <masahirokato.dev@gmail.com>
2019-05-05 13:28:33 +0900

    update(command): implove typings

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,6 @@
 import { identity, AnyFunc, constant } from '@typoerr/atomic'
 
-export type Command<P, M = undefined> = {
+export interface Command<P, M = undefined> {
   type: string
   payload: P
   meta: M
@@ -11,7 +11,7 @@ export interface CommandCreator<T extends any[], P, M = undefined> {
   (...args: T): Command<P, M>
 }
 
-interface WithMetaMapper<T extends any[], R1, R2> {
+interface MapperObject<T extends any[], R1, R2> {
   payload: (...val: T) => R1
   meta?: (...val: T) => R2
 }
@@ -20,7 +20,7 @@ export interface CommandCreatorFactoryResult {
   (type: string): CommandCreator<[undefined?], undefined>
   <P>(type: string): CommandCreator<[P], P>
   <T extends AnyFunc>(type: string, mapper?: T): CommandCreator<Parameters<T>, ReturnType<T>>
-  <T extends any[], P, M>(type: string, mappers?: WithMetaMapper<T, P, M>): CommandCreator<T, P, M>
+  <T extends any[], P, M>(type: string, mappers?: MapperObject<T, P, M>): CommandCreator<T, P, M>
 }
 
 function fromMapper(scope: string, type: string, mapper?: Function) {
@@ -33,7 +33,7 @@ function fromMapper(scope: string, type: string, mapper?: Function) {
   return create
 }
 
-function fromMaperSet(scope: string, type: string, mappers: WithMetaMapper<any, any, any>) {
+function fromMaperObject(scope: string, type: string, mappers: MapperObject<any, any, any>) {
   type = scope + type
   const create = (...args: any[]) => {
     const payload = mappers.payload(...args)
@@ -45,11 +45,11 @@ function fromMaperSet(scope: string, type: string, mappers: WithMetaMapper<any, 
 }
 
 export function factory(scope: string): CommandCreatorFactoryResult {
-  return function creator(type: string, mapper?: AnyFunc | WithMetaMapper<any, any, any>) {
+  return function creator(type: string, mapper?: AnyFunc | MapperObject<any, any, any>) {
     if (typeof mapper === 'function') {
       return fromMapper(scope, type, mapper)
     } else if (typeof mapper === 'object') {
-      return fromMaperSet(scope, type, mapper)
+      return fromMaperObject(scope, type, mapper)
     } else {
       return fromMapper(scope, type)
     }
